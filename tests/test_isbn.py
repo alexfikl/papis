a454967735dc2f0c289c44bcf56648c427128b71
Alexandru Fikl <alexfikl@gmail.com>
2020-02-05 20:04:10 -0600

    add a test case for isbn importer

--- tests/test_isbn.py
+++ tests/test_isbn.py
@@ -1,4 +1,4 @@
-from papis.isbn import *
+from papis.isbn import get_data, Importer
 
 def test_get_data():
     mattuck = get_data(query='Mattuck feynan diagrams')
@@ -6,3 +6,13 @@ def test_get_data():
     assert(isinstance(mattuck, list))
     assert(isinstance(mattuck[0], dict))
     assert(mattuck[0]['isbn-13'] == '9780486670478')
+
+
+def test_match():
+    assert(Importer.match('9780486670478'))
+    assert(Importer.match('this-is-not-an-isbn') is None)
+
+    # NOTE: ISBN for Wesseling - An Introduction to Multigrid Methods
+    importer = Importer.match('9780471930839')
+    assert(importer)
+    assert(importer.uri == '9780471930839')
