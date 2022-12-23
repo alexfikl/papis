9199354ea19bf92c60a49b9b45ab9668a66e97f4
Alexandru Fikl <alexfikl@gmail.com>
2022-12-23 14:38:56 +0200

    database: add a context manager for updating a doc

--- papis/database/__init__.py
+++ papis/database/__init__.py
@@ -1,31 +1,38 @@
-from typing import Optional, Dict
+from contextlib import contextmanager
+from typing import Iterator, Optional
 
 from .base import Database
-from papis.library import Library
+import papis.library
 import papis.logging
+import papis.document
 
 logger = papis.logging.get_logger(__name__)
 
-DATABASES = {}  # type: Dict[Library, Database]
+DATABASES = {}  # type: Dict[papis.library.Library, Database]
 
 
 def get(library_name: Optional[str] = None) -> Database:
     global DATABASES
+
     import papis.config
     if library_name is None:
         library = papis.config.get_lib()
     else:
         library = papis.config.get_lib_from_name(library_name)
-    backend = papis.config.get("database-backend") or "papis"
+
     try:
         database = DATABASES[library]
     except KeyError:
+        backend = papis.config.get("database-backend") or "papis"
         database = _instantiate_database(backend, library)
         DATABASES[library] = database
+
     return database
 
 
-def _instantiate_database(backend_name: str, library: Library) -> Database:
+def _instantiate_database(
+        backend_name: str,
+        library: papis.library.Library) -> Database:
     if backend_name == "papis":
         import papis.database.cache
         return papis.database.cache.Database(library)
@@ -33,7 +40,7 @@ def _instantiate_database(backend_name: str, library: Library) -> Database:
         import papis.database.whoosh
         return papis.database.whoosh.Database(library)
     else:
-        raise Exception("No valid database type: {}".format(backend_name))
+        raise ValueError("Invalid database backend: '{}'".format(backend_name))
 
 
 def get_all_query_string() -> str:
@@ -43,3 +50,14 @@ def get_all_query_string() -> str:
 def clear_cached() -> None:
     global DATABASES
     DATABASES = {}
+
+
+@contextmanager
+def context(doc: papis.document.Document,
+            library_name: Optional[str] = None) -> Iterator[Database]:
+    db = get(library_name=library_name)
+    try:
+        yield db
+    finally:
+        doc.save()
+        db.update(doc)
